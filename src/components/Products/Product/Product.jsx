04d6342843a4aa7d6468096c9c340343d305f880
Ezinne Nnamani <nnamani.ezinne@gmail.com>
2021-03-10 22:25:33 +0100

    Initailized the MartShop React application

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/icons';
+import { AddShoppingCart } from '@material-ui/icons';
+
+const Product = () => {
+    return (
+        <Card>
+            
+        </Card>
+    )
+}
+
+export default Product
